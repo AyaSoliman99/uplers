49d0eb6b9ad97750eba766b208c6c4393b21d2a5
AyaSoliman99 <ayasoliam3799@gmail.com>
2022-07-10 05:19:01 +0200

    adding header for all files

--- js/index.js
+++ js/index.js
@@ -1,81 +1,63 @@
 // ==============creat counter
-let counterSec=document.querySelector(".counters");
+let counterSec = document.querySelector(".counters");
 
 let nums = document.querySelectorAll(".counters .counter h3");
 
-console.log(nums)
+let started = false;
 
-
-let started=false;
-
-window.addEventListener("scroll",function(){
-
-    if(window.scrollY >=counterSec.offsetTop - 300){
-
-        if(!started){
-
-            nums.forEach((num)=>startCount(num));
-
-        }
-
-        started=true;
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= counterSec.offsetTop - 300) {
+    if (!started) {
+      nums.forEach((num) => startCount(num));
     }
+
+    started = true;
+  }
 });
 
-function startCount(el){
+function startCount(el) {
+  let goal = el.dataset.goal;
 
-    let goal =el.dataset.goal;
+  let count = setInterval(() => {
+    el.textContent++;
 
-    let count=setInterval(()=> {
-
-        el.textContent++;
-
-        if(el.textContent == goal){
-
-            clearInterval(count);
-        }
-
-    }, 2000 / goal)
-};
-
+    if (el.textContent == goal) {
+      clearInterval(count);
+    }
+  }, 2000 / goal);
+}
 
 // changing the background color
-let engangeSection=document.querySelector(".engage-us");
+let engangeSection = document.querySelector(".engage-us");
 
-let h3=document.querySelector(".engage-us h3");
+let h3 = document.querySelector(".engage-us h3");
 
-let h4=document.querySelectorAll(".engage-us .text h4");
+let h4 = document.querySelectorAll(".engage-us .text h4");
 
-window.addEventListener("scroll",function(){
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= engangeSection.offsetTop - 200) {
+    engangeSection.classList.add("change-bg");
 
-    if(window.scrollY >= engangeSection.offsetTop-200){
+    h3.style.color = "white";
 
-        engangeSection.classList.add("change-bg");
+    h4.forEach((e) => (e.style.color = "white"));
+  } else {
+    engangeSection.classList.remove("change-bg");
 
-        h3.style.color="white";
+    h3.style.color = "#000";
 
-        h4.forEach(e=>e.style.color="white");
+    h4.forEach((e) => (e.style.color = "#000"));
+  }
 
-    }
-    else
-    {
+  // changing the background after the section
+  if (
+    this.window.scrollY >=
+    engangeSection.nextElementSibling.offsetTop - 200
+  ) {
+    engangeSection.classList.remove("change-bg");
 
-        engangeSection.classList.remove("change-bg");
+    h3.style.color = "#000";
 
-        h3.style.color="#000";
-
-        h4.forEach(e=>e.style.color="#000");
-
-    }
-
-    // changing the background after the section
-    if(this.window.scrollY >= engangeSection.nextElementSibling.offsetTop-200){
-
-        engangeSection.classList.remove("change-bg");
-
-        h3.style.color="#000";
-
-        h4.forEach(e=>e.style.color="#000");
-
-    }
+    h4.forEach((e) => (e.style.color = "#000"));
+  }
 });
--- js/main.js
+++ js/main.js
@@ -1,146 +1,153 @@
-
 // jquery blugins
-$(document).ready(function(){
-// change background color on scroll
-    let header = $("header");
-    $(window).on("scroll", function () {
-        if ($(window).scrollTop() > 10) {
-            if (header.hasClass("change-header-bg")) {
-                return;
-            }
-            header.addClass("change-header-bg");
-        } else {
-            header.removeClass("change-header-bg");
+$(document).ready(function () {
+  // change background color on scroll
+  let header = $("header");
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() > 10) {
+      if (header.hasClass("change-header-bg")) {
+        return;
+      }
+      header.addClass("change-header-bg");
+    } else {
+      header.removeClass("change-header-bg");
     }
-})
+  });
 
+  // scroll to top button
+  let scrollToTop = $(".back-to-top");
 
-// scroll to top button
-let scrollToTop=$(".back-to-top");
-
-$(window).on("scroll",function(){
-
-    if($(window).scrollTop() >= 800){
-
-        if(scrollToTop.is(":hidden")){
-
-            scrollToTop.fadeIn(500);
-
-        }
-
+  $(window).on("scroll", function () {
+    if ($(window).scrollTop() >= 800) {
+      if (scrollToTop.is(":hidden")) {
+        scrollToTop.fadeIn(500);
+      }
+    } else {
+      scrollToTop.fadeOut(500);
     }
-    else{
+  });
+  scrollToTop.on("click", function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      100
+    );
+  });
 
-    scrollToTop.fadeOut(500);
+  // change send-massege content
+  let sendMessage = $(".send-message");
 
+  let sendMessageContent = sendMessage.html();
+
+  sendMessage.on("click", function () {
+    $(this).toggleClass("border");
+
+    if ($(this).html() == sendMessageContent) {
+      sendMessage.html("X");
+
+      $(".send-message-popup").css("transform", "scale(1)");
+    } else {
+      sendMessage.html(sendMessageContent);
+
+      $(".send-message-popup").css("transform", "scale(0)");
     }
-});
-scrollToTop.on("click",function() {
+  });
 
-    $("html, body").animate({
+  // work options menu
+  $(".want-options").on("click", function () {
+    $(this).find("ul").slideToggle(500);
+  });
 
-    scrollTop:0
+  $(".want-options ul li").on("click", function () {
+    let text = $(this).text() + '<i class="fa fa-chevron-down"></i>';
 
-    },100);
-});
+    $(".want-options p").html(text);
+  });
 
+  // more services click
+  let moreInfo = $(".more-info-click");
 
+  let chevronIcon = moreInfo.find("i");
 
-    // change send-massege content 
-    let sendMessage=$(".send-message");
+  let deg = 0;
 
-    let sendMessageContent =sendMessage.html();
+  moreInfo.on("click", function () {
+    moreInfo.parent().parent().next().slideToggle();
 
-    sendMessage.on("click",function(){
+    if (deg == 0) {
+      deg = -180;
 
-        $(this).toggleClass("border");
+      chevronIcon.css("transform", `rotate(${deg}deg)`);
+    } else {
+      deg = 0;
 
-        if($(this).html()==sendMessageContent){
+      chevronIcon.css("transform", `rotate(${deg}deg)`);
+    }
+  });
 
-            sendMessage.html("X");
+  let dg = 0;
 
-            $(".send-message-popup").css("transform","scale(1)");
+  $(".tap-click").on("click", function () {
+    if ($(window).innerWidth() < 768) {
+      $(".tap-click").next().slideUp();
 
-        }
-        else{
+      $(this).next().slideToggle();
+    }
 
-            sendMessage.html(sendMessageContent);
+    if (dg == 0) {
+      dg = -180;
 
-            $(".send-message-popup").css("transform","scale(0)");
-            
-        }
-    });
+      $(".tap-click i").css("transform", `rotate(0deg)`);
 
+      $(this).find("i").css("transform", `rotate(${dg}deg)`);
 
-    // work options menu
-    $(".want-options").on("click",function(){
-        $(this).find("ul").slideToggle(500);
-    });
-
-    $(".want-options ul li").on("click",function(){
-        let text=$(this).text() + "<i class=\"fa fa-chevron-down\"></i>";
-
-        $(".want-options p").html(text);
-    })
-
-
-
-    // more services click
-    let moreInfo=$(".more-info-click");
-
-    let chevronIcon=moreInfo.find("i");
-
-    let deg=0;
-
-    moreInfo.on("click",function(){
-
-        moreInfo.parent().parent().next().slideToggle();
-        
-        if(deg==0){
-
-            deg=-180;
-
-            chevronIcon.css("transform",`rotate(${deg}deg)`);
-
-        }
-        else{
-
-            deg=0;
-
-            chevronIcon.css("transform",`rotate(${deg}deg)`);
-
-        }
-
-    });
-
-
-let dg=0;
-
-    $(".tap-click").on("click",function(){
-
-        if($(window).innerWidth() < 768){
-
-            $(".tap-click").next().slideUp();
-
-            $(this).next().slideToggle();
-
-            }
-
-        if(dg==0){
-
-            dg=-180;
-
-            $(".tap-click i").css("transform",`rotate(0deg)`);
-
-            $(this).find("i").css("transform",`rotate(${dg}deg)`);
-
-            dg=0;
-        }
-    })
+      dg = 0;
+    }
+  });
 });
 
+// showing the menu in small screens
 
+let menuBar = document.querySelector("header .menu-bar");
+const menu = document.querySelector("header .menu");
+console.log(menuBar);
+menuBar.addEventListener("click", function () {
+  if (menu.style.left == "0px") {
+    menu.style.left = "-100%";
+  } else {
+    menu.style.left = "0px";
+  }
+});
 
+//
 
+let links = document.querySelectorAll(".contain-small-menu > a");
+let smallMenu = document.querySelectorAll(".small-menu");
 
+if (window.innerWidth <= 991) {
+  smallMenu.forEach((menu) => {
+    menu.style.cssText = "max-height:0;overflow:hidden;opacity:0;";
+  });
 
+  links.forEach((link) => {
+    link.removeAttribute("href");
+    link.style.cursor = "pointer";
+  });
+
+  links.forEach((link, ind) => {
+    link.addEventListener("click", function () {
+      if (smallMenu[ind].style.overflow == "hidden") {
+        smallMenu[ind].style.cssText =
+          "max-height:100%;overflow:visible;opacity:1;";
+      } else {
+        smallMenu[ind].style.cssText =
+          "max-height:0;overflow:hidden;opacity:0;";
+      }
+    });
+  });
+} else {
+  links.forEach((link) => {
+    link.setAttribute("href", link.dataset.href);
+    link.style.cursor = "pointer";
+  });
+}
